19af1846f575a5b732e2809ab7c0426900bb0d0b
githubofsanren <1002059187@qq.com>
2021-09-06 22:34:03 +0800

    删除无用的，准备自动生成

--- changgou-parent/changgou-service/changgou-service-goods/src/test/java/com/changgou/BrandDaoTest.java
+++ changgou-parent/changgou-service/changgou-service-goods/src/test/java/com/changgou/BrandDaoTest.java
@@ -1,7 +1,6 @@
 package com.changgou;
 
 import com.changgou.goods.dao.BrandMapper;
-import com.changgou.goods.pojo.Brand;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
